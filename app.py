3e6e730147faed510c6daf4bbc0ba40b7c4f87b6
Etienne <etienne.allagnat@gmail.com>
2026-01-09 13:59:50 +0100

    garphonglet

--- app.py
+++ app.py
@@ -6,8 +6,17 @@ df = load_data("airbnb_tp.csv")
 
 st.title("Dashboard Airbnb")
 
-median_price, mean_price = compute_price_stats(df)
-st.metric("Prix médian par personne", f"{median_price:.2f} €")
+tab1, tab2, tab3 = st.tabs(["📊 Overview", "📈 Analyses", "🗂 A définir"])
 
-st.pyplot(plot_price_distribution(df))
-st.pyplot(plot_capacity(df))
+with tab1:
+    st.header("📊 Overview")
+    median_price, mean_price = compute_price_stats(df)
+    st.metric("Prix médian par personne", f"{median_price:.2f} €")
+
+with tab2:
+    st.header("Analyses")
+    st.pyplot(plot_price_distribution(df))
+
+with tab3:
+    st.header("🗂 A définir")
+    st.write("Ici tu mets tes analyses avancées…")
